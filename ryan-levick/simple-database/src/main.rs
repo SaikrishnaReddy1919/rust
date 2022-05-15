7f07dd5de5ffb80ee1baae1f6536e0f8e7764a28
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-05-15 17:48:43 +0530

    more clean way of doing flush

--- ryan-levick/simple-database/src/main.rs
+++ ryan-levick/simple-database/src/main.rs
@@ -2,9 +2,9 @@ use std::collections::HashMap;
 
 fn main() {
     // first argument is file path to executable file so skip 1st one
-    let mut arguments = std::env::args().skip(1); 
+    let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("key was not there");
-    let value = arguments.next().unwrap(); 
+    let value = arguments.next().unwrap();
 
     // expect() -> says compiler to crash on error.
     // try removing .expect(), program wont crash.
@@ -12,11 +12,16 @@ fn main() {
 
     database.insert(key.to_uppercase(), value.clone());
     database.insert(key, value);
+    match database.flush() {
+        Ok(()) => println!("Db flushed successfully."),
+        Err(e) => println!("Handle Error here:{}", e)
+    }
 }
 
 #[derive(Debug)]
 struct Database {
     map: HashMap<String, String>,
+    flush : bool
 }
 
 impl Database {
@@ -45,40 +50,49 @@ impl Database {
             map.insert(key.to_owned(), value.to_owned());
         }
 
-        Ok(Database { map })
+        Ok(Database { map, flush: false })
     }
 
-    fn insert(&mut self, key:String, value:String) {
+    fn insert(&mut self, key: String, value: String) {
         self.map.insert(key, value);
     }
 
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.flush = true;
+        do_flush(&self)
+    }
 }
 
 // what if we want to call flush after inserting automatically.
-// That's where drop comes in...if we impl Drop trait for Database, we can use drop inside Drop and 
+// That's where drop comes in...if we impl Drop trait for Database, we can use drop inside Drop and
 // do whatever we want to do(the code inside flush method) inside drop method.
 
 impl Drop for Database {
     fn drop(&mut self) {
-        let mut contents = String::new();
-
-        //try removing & to self.map
-        for (key, value) in &self.map {
-            // method 1 - not efficient
-            //reason : push_str is taking red to kvpair...what happens to the created memory after after it is used in push_str ?
-            // let kvpair = format!("{}\t{}\n", key, value);
-            // contents.push_str(&kvpair);
-
-            //method - 2 : we can also do same without creating new memory for kvpair
-            // efficient
-            contents.push_str(key);
-            contents.push('\t');
-            contents.push_str(value);
-            contents.push('\n');
-
-            
+        // calls do_flush only if self.flush is not true
+        if !self.flush {
+            let _ = do_flush(&self);
         }
-        // _ means -> keep the result in _ and ignore it.
-        let _ = std::fs::write("kv.db", contents);
     }
+}
+
+fn do_flush(database : &Database) -> Result<(), std::io::Error> {
+    let mut contents = String::new();
+
+    //try removing & to self.map
+    for (key, value) in &database.map {
+        // method 1 - not efficient
+        //reason : push_str is taking red to kvpair...what happens to the created memory after after it is used in push_str ?
+        // let kvpair = format!("{}\t{}\n", key, value);
+        // contents.push_str(&kvpair);
+
+        //method - 2 : we can also do same without creating new memory for kvpair
+        // efficient
+        contents.push_str(key);
+        contents.push('\t');
+        contents.push_str(value);
+        contents.push('\n');
+    }
+    // _ means -> keep the result in _ and ignore it.
+    std::fs::write("kv.db", contents)
 }
